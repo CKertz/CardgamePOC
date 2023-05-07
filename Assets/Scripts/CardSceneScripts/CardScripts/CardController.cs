7c0426380ef344748fe77a89a07fe72cd88ce7f6
Cooper Kertz <kertzc@live.com>
2023-05-07 17:12:22 -0500

    added drag/drop ability to cards in scene

--- Assets/Scripts/CardSceneScripts/CardScripts/CardController.cs
+++ Assets/Scripts/CardSceneScripts/CardScripts/CardController.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class CardController : MonoBehaviour
 {
+    private Vector3 mousePostiionOffset;
     private float lastClickTime = 0f;
     private float doubleClickTimeThreshold = 0.3f;
+
+    private float spawnedYCoordinate;
+    private float spawnedXCoordinate;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(transform.name);
+        spawnedXCoordinate = transform.localPosition.x;
+        spawnedYCoordinate = transform.localPosition.y;
+
+        Debug.Log("x:" + spawnedXCoordinate + " y:" + spawnedYCoordinate);
+
     }
 
     // Update is called once per frame
@@ -29,5 +38,37 @@ public class CardController : MonoBehaviour
             //Destroy(gameObject);
         }
         lastClickTime = Time.time;
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDown()
+    {
+        mousePostiionOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + mousePostiionOffset;
+        if (transform.localPosition.y > -0.3)
+        {
+            Debug.Log("in consume zone");
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        //if y position is > -0.2, consume card else snap back to original location
+        if(transform.localPosition.y > -0.4)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(spawnedXCoordinate, spawnedYCoordinate);
+        }
     }
 }
