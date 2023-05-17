e7c2cf97df0f2cb986269872842858a33f974523
Cooper Kertz <kertzc@live.com>
2023-05-16 20:03:10 -0500

    refactor moving logic into OrderController

--- Assets/Scripts/CardSceneScripts/CustomerScripts/CustomerController.cs
+++ Assets/Scripts/CardSceneScripts/CustomerScripts/CustomerController.cs
@@ -54,38 +54,14 @@ public class CustomerController : MonoBehaviour
         // start taking order
         // start waitingforfood timer
         var orderPrefab = transform.Find("prefab_CustomerOrder");
-        var orderBackgroundObject = orderPrefab.transform.Find("OrderBackground");
-        var orderBackgroundSprite = orderBackgroundObject.GetComponent<SpriteRenderer>();
-
-        orderBackgroundSprite.enabled = true;
-        orderBackgroundSprite.transform.localPosition = new Vector3(4, 0.1f);
-
-
         var orderScript = orderPrefab.GetComponent<OrderController>();
-        var orderItemSprites = new List<string>();
-        orderItemSprites = orderScript.GetOrderItemSprites(customer);
-        
-        for(int i = 0; i < orderItemSprites.Count; i++)
-        {
-            Debug.Log("OrderItem" + i);
-            var currentOrderItem = orderBackgroundObject.transform.Find("OrderItem" + i).GetComponent<SpriteRenderer>();
-            currentOrderItem.enabled = true;
+        orderScript.EnableOrder(orderPrefab, customer);
 
-            Debug.Log("currentorderitem:" + currentOrderItem.name);
-            if (currentOrderItem != null)
-            {
-                Debug.Log("spritepath:"+orderItemSprites[i]);
-                Sprite sprite = Resources.Load<Sprite>(orderItemSprites[i]);
-                currentOrderItem.sprite = sprite;
-
-            }
-        }
-
-        StartCoroutine(MoveCustomerCoroutine());
+        StartCoroutine(MoveCustomerCoroutine(orderScript));
         orderPrefab.SetParent(null);
     }
 
-    private IEnumerator MoveCustomerCoroutine()
+    private IEnumerator MoveCustomerCoroutine(OrderController orderScript)
     {
         var orderPrefab = transform.Find("prefab_CustomerOrder");
         while (transform.localPosition.x > -4)
@@ -97,43 +73,6 @@ public class CustomerController : MonoBehaviour
             yield return null;
 
         }
-        OnCustomerOutOfSceneFinished(orderPrefab);
+        orderScript.OnCustomerOutOfSceneFinished(orderPrefab);
     }
-
-    private void OnCustomerOutOfSceneFinished(Transform orderPrefab)
-    {
-        StartCoroutine(MoveOrderCoroutine(orderPrefab));
-    }
-
-    private IEnumerator MoveOrderCoroutine(Transform orderPrefab)
-    {     
-        while (orderPrefab.transform.localPosition.x < -2f)
-        {
-            Vector3 newPosition = orderPrefab.position + Vector3.right * speed * Time.deltaTime * 1.2f;
-            orderPrefab.transform.position = newPosition;
-
-            yield return null;
-
-        }
-        OnOrderOutOfSceneFinished(orderPrefab);
-    }
-    private void OnOrderOutOfSceneFinished(Transform orderPrefab)
-    {
-        orderPrefab.localScale = new Vector3(0.25f, 0.25f);
-        orderPrefab.localPosition = new Vector3(-2.04f, 0.43f);
-        var background = orderPrefab.transform.Find("OrderBackground");
-        var backgroundSprite = background.GetComponent<SpriteRenderer>();
-        backgroundSprite.sortingOrder = 2;
-
-        AdjustOrderItemIconSortingOrder(background);
-    }
-    private void AdjustOrderItemIconSortingOrder(Transform orderPrefab)
-    {
-        for(int i = 0; i < customer.CustomerOrder.Count; i++)
-        {
-            var currentOrderItem = orderPrefab.transform.Find("OrderItem" + i).GetComponent<SpriteRenderer>();
-            currentOrderItem.sortingOrder = 3;
-        }
-    }
-
 }
--- Assets/Scripts/CardSceneScripts/OrderController.cs
+++ Assets/Scripts/CardSceneScripts/OrderController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class OrderController : MonoBehaviour
 {
-
+    public int orderItems = 3;
+    public float speed = 2.0f;
 
     public List<string> GetOrderItemSprites(Customer customer)
     {
@@ -18,5 +19,73 @@ public class OrderController : MonoBehaviour
         return customerOrderItemSprites;
     }
 
+    public void LoadOrderItemSprites(List<string> orderItemSprites, Transform orderBackgroundObject)
+    {
+        for (int i = 0; i < orderItemSprites.Count; i++)
+        {
+            Debug.Log("OrderItem" + i);
+            var currentOrderItem = orderBackgroundObject.transform.Find("OrderItem" + i).GetComponent<SpriteRenderer>();
+            currentOrderItem.enabled = true;
+
+            Debug.Log("currentorderitem:" + currentOrderItem.name);
+            if (currentOrderItem != null)
+            {
+                Debug.Log("spritepath:" + orderItemSprites[i]);
+                Sprite sprite = Resources.Load<Sprite>(orderItemSprites[i]);
+                currentOrderItem.sprite = sprite;
+
+            }
+        }
+    }
+    public void OnCustomerOutOfSceneFinished(Transform orderPrefab)
+    {
+        Debug.Log(orderPrefab.name);
+        orderPrefab.SetParent(null);
+        StartCoroutine(MoveOrderCoroutine(orderPrefab));
+    }
+
+    private IEnumerator MoveOrderCoroutine(Transform orderPrefab)
+    {
+        while (orderPrefab.transform.localPosition.x < -5f)
+        {
+            Vector3 newPosition = orderPrefab.position + Vector3.right * speed * Time.deltaTime * 1.2f;
+            orderPrefab.transform.position = newPosition;
+
+            yield return null;
+
+        }
+        OnOrderOutOfSceneFinished(orderPrefab);
+    }
+
+    private void OnOrderOutOfSceneFinished(Transform orderPrefab)
+    {
+        orderPrefab.localScale = new Vector3(0.25f, 0.25f);
+        orderPrefab.localPosition = new Vector3(-2.04f, 0.43f);
+        var background = orderPrefab.transform.Find("OrderBackground");
+        var backgroundSprite = background.GetComponent<SpriteRenderer>();
+        backgroundSprite.sortingOrder = 2;
+
+        AdjustOrderItemIconSortingOrder(background);
+    }
+    private void AdjustOrderItemIconSortingOrder(Transform orderPrefab)
+    {
+        for (int i = 0; i < orderItems; i++)
+        {
+            var currentOrderItem = orderPrefab.transform.Find("OrderItem" + i).GetComponent<SpriteRenderer>();
+            currentOrderItem.sortingOrder = 3;
+        }
+    }
+
+    public void EnableOrder(Transform orderPrefab, Customer customer)
+    {
+        var orderBackgroundObject = orderPrefab.transform.Find("OrderBackground");
+        var orderBackgroundSprite = orderBackgroundObject.GetComponent<SpriteRenderer>();
+
+        orderBackgroundSprite.enabled = true;
+        orderBackgroundSprite.transform.localPosition = new Vector3(4, 0.1f);
+
+        var orderItemSprites = GetOrderItemSprites(customer);
+        LoadOrderItemSprites(orderItemSprites, orderBackgroundObject);
+    }
 
 }
